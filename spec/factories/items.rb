6b6b00cb029e029ded79b51697c0bab491fd1c55
n0918k <ellvin0918@gmail.com>
2020-08-31 19:49:39 +0900

    単体テストコードの実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
   factory :item do
+    association :user
+    association :prefecture
+    association :item_status
+    association :shipping_day
+    association :category
+
     name {'あああ'}
-    image {''}
+    instruction{'あああ'}
+    postage_id{'1'}
+    price{'500'}
+    category_id{'1'}
+    shipping_day_id{'1'}
+    item_status_id{'1'}
+    prefecture_id{'1'}
     
   end
 end
