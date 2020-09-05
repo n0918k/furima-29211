270fdbe6442f4a380e3199b9cc08d5a84d7b06dd
n0918k <ellvin0918@gmail.com>
2020-09-05 12:35:27 +0900

    test1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,6 +28,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
+
     @item = Item.find(params[:item_id])
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
