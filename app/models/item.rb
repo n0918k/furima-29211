ef12d0c0597bd39603acba8f30aec23ad5ecbba3
n0918k <ellvin0918@gmail.com>
2020-09-24 09:22:17 +0900

    update

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_status
   has_one_attached :image
   belongs_to :user
-  has_one :order
+  has_one :order, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   with_options presence: true do
